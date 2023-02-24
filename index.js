586bd3e777d7e3963b3d5ae42685d9c02f865c5d
Amro-Elata <amroelata@gmail.com>
2023-02-24 20:40:09 +0000

    Created Index page

--- index.js
+++
@@ -1,5 +0,0 @@
-const banner = document.querySelector(".banner")
-window.addEventListener("scroll", function(){
-    const value = 1000 - window.scrollY;
-    banner.style.clipPath = "circle("+ value +"px at center center)";
-})
